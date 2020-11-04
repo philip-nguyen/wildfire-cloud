8813c90274efb129dc516d817e4d0e5956383aff
Philip <vietwhammies@gmail.com>
2020-11-03 22:46:01 -0800

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+
+  render() {
+    return (
+        <div className="ui container" style={{ marginTop: '10px'}}>
+            Wildfire Cloud Solution (:
+        </div>
+    );
+}
+
+}
+
+export default App;
