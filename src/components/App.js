a0e029f294af3deb33f263779c2aa88ffa483348
ejrbrand <skylar_skylar101@yahoo.com>
2020-11-08 20:32:57 -0800

    implemented google maps

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,48 @@
 import React from 'react';
+import styled from 'styled-components';
+import { GoogleMap, withScriptjs, withGoogleMap} from "react-google-maps";
 
-class App extends React.Component {
+function clickMe(){
+  alert('You clicked me!');
+}
 
-  render() {
-    return (
-        <div className="ui container" style={{ marginTop: '10px'}}>
-            Wildfire Cloud Solution (:
-        </div>
+function Map() {
+  return (
+    <GoogleMap
+      defaultZoom = {10}
+      defaultCenter ={{ lat: 37.338207, lng: -121.886330}}
+      />
     );
 }
 
-}
+const WrappedMap = withScriptjs(withGoogleMap(Map));
 
-export default App;
+const Button = styled.button`
+  background-color: #e0e0e0;
+  color: black;
+  padding: 5px 15px;
+  border-radius: 5px;
+  outline: 0;
+  cursor: pointer;
+  box-shadow: 0px 2px 2px lightgray;
+  transition: ease background-color 250ms;
+  &:hover {
+    background-color: #283493;
+  }
+`
+
+export default function App() {
+    return (
+      <div style = {{width: '100vw', height: '100vh'}}>
+        <WrappedMap
+          googleMapURL = {`https://maps.googleapis.com/maps/api/js?v=3.exp&
+            libraries=geometry,draving,places&key=${
+              process.env.REACT_APP_GOOGLE_KEY
+            }`}
+          loadingElement = {<div style = {{ height: "100%"}} />}
+          containerElement = {<div style = {{ height: "100%" }} />}
+          mapElement = {<div style = {{ height: "100%"}} />}
+        />
+    </div>
+    );
+  }
---
+++ src/components/wildfire-prediction/README.md
@@ -0,0 +1,7 @@
+#Install Requirements
+
+Type the following commands
+`npm install styled-components --save`
+`npm install --save google-maps-react`
+
+#GooglemapURL = https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,draving,places
