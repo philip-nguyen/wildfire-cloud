d40368cfdf6d9ba4e5a3bb8a35028301064ff36c
Philip <philip.nguyen@sjsu.edu>
2020-11-10 20:16:11 -0800

    used semantic ui icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,4 @@
 import React, {useState} from 'react';
-import * as FaIcons from 'react-icons/fa';
-import * as AiIcons from 'react-icons/ai';
 import { Link } from 'react-router-dom';
 import { SidebarData } from './SidebarData';
 import './Navbar.css';
@@ -16,14 +14,14 @@ function Navbar() {
     <IconContext.Provider value={{color: '#fff'}}>
       <div className = "navbar">
         <Link to="#" className='menu-bars'>
-          <FaIcons.FaBars onClick={showSidebar}/>
+          <i className="list icon" onClick={showSidebar}/>
         </Link>
       </div>
       <nav className = {sidebar ? 'nav-menu active' : 'nav-menu'}>
         <ul className = 'nav-menu-items' onClick={showSidebar}>
           <li className = "navbar-toggle">
             <Link to = "#" className = 'menu-bars'>
-              <AiIcons.AiOutlineClose />
+            <i className="window close icon"></i>
             </Link>
           </li>
           {SidebarData.map((item, index) => {
--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -1,34 +1,31 @@
 import React from 'react'
-import * as FaIcons from 'react-icons/fa'
-import * as AiIcons from 'react-icons/ai'
-import * as IoIcons from 'react-icons/io'
 
 export const SidebarData = [
   {
     title: 'Home',
     path: '/',
-    icon: <AiIcons.AiFillHome />,
+    icon: <i className="balance scale icon"/>,
     cName: 'nav-text'
   },
 
   {
     title: 'WildfireAnalysis',
     path: '/WildfireAnalysis',
-    icon: <IoIcons.IoMdLocate />,
+    icon: <i className="search icon"/>,
     cName: 'nav-text'
   },
 
   {
     title: 'WildfireDetection',
     path: '/WildfireDetection',
-    icon: <IoIcons.IoIosFlame />,
+    icon: <i className="fire icon"/>,
     cName: 'nav-text'
   },
 
   {
     title: 'WildfirePrediction',
     path: '/WildfirePrediction',
-    icon: <IoIcons.IoMdFastforward />,
+    icon: <i className="forward icon"/>,
     cName: 'nav-text'
   }
 ]
